62ed61a560f91d5a6496b1b19eca82e547cdeb34
Barbara <bar@Barbaras-MacBook-Air.local>
2018-01-15 21:57:22 -0300

    Primera resolución

---
+++ Clase 01/Alumno/Ejercicio/index.js
@@ -0,0 +1,34 @@
+// Crear una función constructora llamada User con las siguientes características:
+// Debe tener las propiedades privadas, que deben ser recibidas al momento de crear el elemento:
+
+// firstName String
+// age Number
+// isAdmin Boolean (Valor por defecto false)
+// telephones Array (Valor por defecto Array vacío)
+
+// function User(firstName, age, isAdmin, telephones) {
+//   this.showName = function () {
+//     return firstName
+//   }
+//   this.showAge = function () {
+//     return age
+//   }
+// }
+
+// var usuario = new User('Pedro', 18, false)
+
+// console.log(usuario.showName())
+// console.log(usuario.showAge())
+
+// // Una función publica que permita agregar teléfonos al usuario. // con push
+
+// // Una función publica que permita mostrar todas las propiedades del usuario en consola, mostrando los campos firstName, lastName y telephones en el mismo String.
+
+// function showUser(usuario) {
+//   console.log(usuario.showName(), usuario.showAge())
+// }
+
+// showUser(usuario)
+
+// resolución
+
